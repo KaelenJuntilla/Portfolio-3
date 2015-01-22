80a6777bafee1b33d45b18d5a6718bbc0a61e396
Kaelen Juntilla <Da4kast@charter.net>
2015-01-21 20:31:01 -0800

    11th push

--- public_html/script.js
+++ public_html/script.js
@@ -6,13 +6,64 @@
 
 
 $("document").ready(function(){
-   $('#toggleText').bind('click', toggleTheText);
+   $("#yeah").css("background-color", "cyan");
+    
+    $('#last').css({"background-color": "purple", "color": "white"});
+    
+    $('#sibling').bind('click', alertButtonClick);
+    
+    $("#heading1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $('#heading4').bind('click', mouseClick);
+    
+    $('#replaceWText').bind('click', replaceWText);
+    
+    $('#randPara'). bind('click', addPara);
+    
+    $('#toggleText').bind('click', toggleTheText);
+    
+    $("#yeah").accordion({ header: "h4"});
    
     
    
 });
 
-function toggleTheText () {
-    $('p').toggle(2500);
+function alertButtonClick() {
+    alert("There was a button clicked");
 }
+
+function mouseOverMe() {
+    $("#heading1").html('This is easy');
+}
+
+function mouseOutMe() {
+    $('#heading1').html('I dont know what to do');
+}
+
+function mouseClick() {
+    $('p').html('iusdhfiuhiudhfihwieuhfweiufhwiuefhiuwhfiuhuiehfiuwefh');
+}
+
+function replaceWText() {
+    $('#replaceWText').text('Replaced');
+}
+
+function addPara() {
+    $('#randPara').append('<p>ADDED<p>');
+}
+
+
+function slideTheImage() {
+    $('second').slideUp(2500);
+}
+
+function unslideTheImage() {
+    $('second').slideDown(2500);
+}
+
+function toggleTheText() {
+    $('#heading4').toggle(2500);
+}
+
+
 
